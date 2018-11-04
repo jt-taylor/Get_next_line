d73069ac3a99a21a14cd3550cb5233d86fa6578b
Jackson Taylor <jtaylor@e1z3r8p2.42.us.org>
2018-11-04 10:03:46 -0800

    does this work for the embedded repo libft?

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtaylor <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/27 10:11:01 by jtaylor           #+#    #+#             */
+/*   Updated: 2018/11/01 11:14:12 by jtaylor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include "libft.h"
+
+int	get_next_line(const int fd, char **line);
+
+#endif
